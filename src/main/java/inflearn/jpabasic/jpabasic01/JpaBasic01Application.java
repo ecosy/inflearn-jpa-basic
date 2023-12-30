64aed9e30550f42ed2d67fc26376e1e6a27ce2d0
ecosy <kevin9434@naver.com>
2023-12-30 15:29:16 +0900

    [2장 JPA 시작하기] entity manager 세팅

---
+++ src/main/java/inflearn/jpabasic/jpabasic01/JpaBasic01Application.java
@@ -0,0 +1,39 @@
+package inflearn.jpabasic.jpabasic01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+@SpringBootApplication
+public class JpaBasic01Application {
+
+    public static void main(String[] args) {
+//		SpringApplication.run(JpaBasic01Application.class, args);
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Member member = new Member();
+            member.setId(1L);
+            member.setName("name");
+
+            em.persist(member);
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
